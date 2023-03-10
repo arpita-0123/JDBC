c0aaab009fb2639789485179c8bf7d1535ca03f2
home <home@home-PC>
2023-03-10 01:20:55 -0500

    jdbc

--- jdbc/src/main/java/jdbc/JdbcDemo.java
+++ jdbc/src/main/java/jdbc/JdbcDemo.java
@@ -5,8 +5,7 @@ public class JdbcDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World");
-		System.out.println("New Code");
+		System.out.println("Hello");
 	}
 
 }
